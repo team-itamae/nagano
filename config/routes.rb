8892c39d9c5d8a0f97767729e7c950cfa15858dc
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-16 12:23:55 +0000

    [update] admin/routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,42 +26,7 @@ Rails.application.routes.draw do
     get 'sessions/create'
     get 'sessions/destroy'
   end
-  namespace :admin do
-    patch 'making_status/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    patch 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    patch 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
+    namespace :public do
     get 'customers/show'
     get 'customers/edit'
     get 'customers/update'
@@ -81,6 +46,42 @@ Rails.application.routes.draw do
     get 'homes/top'
     get 'homes/about'
   end
+  namespace :admin do
+    patch 'making_status/update' => "/admin/making_status/:id"
+  end
+  namespace :admin do
+    get 'orders/show'  => "/admin/orders/:id"
+    patch 'orders/update'  => "/admin/orders/:id"
+  end
+  namespace :admin do
+    get 'customers/index' => "/admin/customers"
+    get 'customers/show' => "/admin/customers/:id"
+    get 'customers/edit' => "/admin/customers/:id/edit"
+    patch 'customers/update' => "/admin/customers/:id"
+  end
+  namespace :admin do
+    get 'genres/index' => "/admin/genres"
+    post 'genres/create'=> "/admin/genres"
+    get 'genres/edit'=> "/admin/genres/:id/edit"
+    patch 'genres/update' => "/admin/genres/:id"
+  end
+  namespace :admin do
+    get 'items/index' => "/admin/items"
+    get 'items/new' => "/admin/items/new"
+    post 'items/create' => "/admin/items"
+    get 'items/show' => "/admin/items/:id"
+    get 'items/edit' => "/admin/items/:id/edit"
+    patch 'items/update' => "/admin/items/:id"
+  end
+  namespace :admin do
+    get 'homes/top' => "/admin"
+  end
+  namespace :admin do
+    get 'sessions/new' => "/admin/sign_in"
+    post 'sessions/create' => "/admin/sign_in"
+    delete 'sessions/destroy' => "/admin/sign_out"
+  end
+
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
