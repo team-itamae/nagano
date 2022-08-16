72330d3ef14ed7ae82ffcdbc924bc5ee4725810b
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-16 11:23:49 +0000

    [Finish] admin/controller

---
+++ app/controllers/admin/making_status_controller.rb
@@ -0,0 +1,4 @@
+class Admin::MakingStatusController < ApplicationController
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,39 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    patch 'making_status/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+    patch 'orders/update'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    patch 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
