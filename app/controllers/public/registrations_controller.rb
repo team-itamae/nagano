1524910f6ec2ba299dabaa454d2882e32ac06863
sora <sora621621@icloud.com>
2022-08-16 11:24:24 +0000

    public

---
+++ app/controllers/public/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Public::RegistrationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'registrations/new'
+    get 'registrations/create'
+    get 'registrations/destroy'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
