bc85bff2de510bcc64274a1312651df7dfad33e6
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-17 09:52:39 +0000

    [Finish] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,68 +1,56 @@
 Rails.application.routes.draw do
-  
+
   namespace :admin do
     get 'making_status/update'
   end
   namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
+    resources :orders, only: [:show, :update]
   end
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    resources :customers, only: [:index, :show, :edit, :update]
   end
+
   namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
+  resources :genres, only: [:index, :edit, :create, :update]
   end
+
   namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
+    resources :items, only: [:index, :show, :create, :new, :edit, :update]
   end
-  namespace :public do
-    get 'adresses/index'
-    get 'adresses/edit'
-    get 'adresses/create'
-    get 'adresses/update'
-    get 'adresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
+
+
+    get 'adresses/index' => 'public/adresses#index'
+    get 'adresses/edit' => 'public/adresses#edit'
+    post 'adresses/create' => 'public/adresses#create'
+    patch 'adresses/update' => 'public/adresses#update'
+    delete 'adresses/destroy' => 'public/adresses#destroy'
+
+    get 'orders/new' => 'public/orders#new'
+    post 'orders/confirm' => 'public/orders#confirm'
+    get 'orders/complete' => 'public/orders#complete'
+    post 'orders/create' => 'public/orders#create'
+    get 'orders/index' => 'public/orders#index'
+    get 'orders/show' => 'public/orders#show'
+
+    get 'cart_items/index' => 'public/cart_items#index'
+    patch 'cart_items/update' => 'public/cart_items#update'
+    delete 'cart_items/destroy' => 'public/cart_items#destroy'
+    delete 'cart_items/destroy_all' => 'public/cart_items#destroy_all'
+    post 'cart_items/create' => 'public/cart_items#create'
+
+
+    get 'customers/show' => 'public/customers#show'
+    get 'customers/edit' => 'public/customers#edit'
+    patch 'customers/update' => 'public/customers#update'
+    get 'customers/unsubscribe' => 'public/customers#unsubscribe'
+    patch 'customers/withdraw' => 'public/customers#withdraw'
+
+    get 'items/index' => 'public/items#index'
+    get 'items/show' => 'public/items/#show'
+
   get '/' => "public/homes#top"
   get '/about' => "public/homes#about"
- 
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
