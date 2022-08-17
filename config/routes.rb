65501fbd06f0e6726697c08ee9bf4176a8e06d33
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-17 06:12:37 +0000

    [Change]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,84 +1,108 @@
 Rails.application.routes.draw do
   namespace :public do
-    get 'adresses/index' => "/adresses"
-    get 'adresses/edit' => "/adresses/:id/edit"
-    get 'adresses/create' => "/adresses"
-    get 'adresses/update' => "/adresses/:id"
-    get 'adresses/destroy' => "/adresses/:id"
+    # get 'adresses/index' => "/adresses"
+    # get 'adresses/edit' => "/adresses/:id/edit"
+    # get 'adresses/create' => "/adresses"
+    # get 'adresses/update' => "/adresses/:id"
+    # get 'adresses/destroy' => "/adresses/:id"
+    resources :adresses, only:[:index, :create, :edit, :update, :destroy]
   end
+
   namespace :public do
-    get 'orders/new' => "/orders/new"
-    get 'orders/confirm' => "/orders/confirm"
-    get 'orders/complete' => "/orders/complete"
-    get 'orders/create' => "/orders"
-    get 'orders/index' => "/orders"
-    get 'orders/show' => "/orders/:id"
+    # get 'orders/new' => "/orders/new"
+    get '/orders/confirm' => "orders#confirm"
+    get '/orders/complete' => "orders#complete"
+    # get 'orders/create' => "/orders"
+    # get 'orders/index' => "/orders"
+    # get 'orders/show' => "/orders/:id"
+    resources :adresses, only:[:index, :create, :show, :new]
   end
+
   namespace :public do
-    get 'cart_items/index' => "/cart_items"
-    get 'cart_items/update' => "/cart_items/:id"
-    get 'cart_items/destroy' => "/cart_items/:id"
-    get 'cart_items/destroy_all' => "/cart_items/destroy_all"
-    get 'cart_items/create' => "/cart_items"
+    # get 'cart_items/index' => "/cart_items"
+    # get 'cart_items/update' => "/cart_items/:id"
+    # get 'cart_items/destroy' => "/cart_items/:id"
+    get '/cart_items/destroy_all' => "cart_items#destroy_all"
+    # get 'cart_items/create' => "/cart_items"
+    resources :cart_items, only:[:index, :create, :update, :destroy]
   end
+
   namespace :public do
-    get 'sessions/new' => "/customers/sign_in"
-    get 'sessions/create' => "/customers/sign_in"
-    get 'sessions/destroy' => "/customers/sign_out"
+    get '/customers/sign_in' => "sessions#new"
+    get '/customers/sign_in' => "sessions#create"
+    get '/customers/sign_out' => "sessions#destroy"
   end
-    namespace :public do
-    get 'customers/show' => "/customers"
-    get 'customers/edit' => "/customers/edit"
-    get 'customers/update' => "/customers"
-    get 'customers/unsubscribe' => "/customers/unsubscribe"
-    get 'customers/withdraw' => "/customers/withdraw"
-  end
+
   namespace :public do
-    get 'registrations/new' => "/customers/sign_up"
-    get 'registrations/create' => "//customers"
+    # get 'customers/show' => "/customers"
+    # get 'customers/edit' => "/customers/edit"
+    # get 'customers/update' => "/customers"
+    get '/customers/unsubscribe' => "customers#unsubscribe"
+    get '/customers/withdraw' => "customers#withdraw"
+    resources :customers, only:[:show, :edit, :update]
   end
+
   namespace :public do
-    get 'items/index' => "/items"
-    get 'items/show' => "/items/:id"
+    get '/customers/sign_up' => "registrations#new"
+    get '/customers' => "registrations#create"
   end
+
   namespace :public do
-    get 'homes/top' => "/"
-    get 'homes/about' => "/about"
+    # get 'items/index' => "/items"
+    # get 'items/show' => "/items/:id"
+    resources :items, only:[:index, :show]
   end
-  namespace :admin do
-    patch 'making_status/update' => "/admin/making_status/:id"
+
+ namespace :public do
+    get '/' => "homes#top"
+    get '/about' => "homes#about"
   end
+
+
   namespace :admin do
-    get 'orders/show'  => "/admin/orders/:id"
-    patch 'orders/update'  => "/admin/orders/:id"
+    patch '/admin/making_status/:id' => "making_status#update"
   end
+
   namespace :admin do
-    get 'customers/index' => "/admin/customers"
-    get 'customers/show' => "/admin/customers/:id"
-    get 'customers/edit' => "/admin/customers/:id/edit"
-    patch 'customers/update' => "/admin/customers/:id"
+    # get 'orders/show'  => "/admin/orders/:id"
+    # patch 'orders/update'  => "/admin/orders/:id"
+    resources :orders, only:[:show, :update]
   end
+
   namespace :admin do
-    get 'genres/index' => "/admin/genres"
-    post 'genres/create'=> "/admin/genres"
-    get 'genres/edit'=> "/admin/genres/:id/edit"
-    patch 'genres/update' => "/admin/genres/:id"
+    # get 'customers/index' => "/admin#customers"
+    # get 'customers/show' => "/admin/customers/:id"
+    # get 'customers/edit' => "/admin/customers/:id/edit"
+    # patch 'customers/update' => "/admin/customers/:id"
+    resources :customers, only:[:index, :edit, :update, :show]
   end
+
   namespace :admin do
-    get 'items/index' => "/admin/items"
-    get 'items/new' => "/admin/items/new"
-    post 'items/create' => "/admin/items"
-    get 'items/show' => "/admin/items/:id"
-    get 'items/edit' => "/admin/items/:id/edit"
-    patch 'items/update' => "/admin/items/:id"
+    # get 'genres/index' => "/admin/genres"
+    # post 'genres/create'=> "/admin/genres"
+    # get 'genres/edit'=> "/admin/genres/:id/edit"
+    # patch 'genres/update' => "/admin/genres/:id"
+    resources :genres, only:[:index, :create, :edit, :update ]
   end
+
   namespace :admin do
-    get 'homes/top' => "/admin"
+    # get 'items/index' => "/admin/items"
+    # get 'items/new' => "/admin/items/new"
+    # post 'items/create' => "/admin/items"
+    # get 'items/show' => "/admin/items/:id"
+    # get 'items/edit' => "/admin/items/:id/edit"
+    # patch 'items/update' => "/admin/items/:id"
+    resources :items, only:[:index, :create, :edit, :update, :show, :new]
   end
+
   namespace :admin do
-    get 'sessions/new' => "/admin/sign_in"
-    post 'sessions/create' => "/admin/sign_in"
-    delete 'sessions/destroy' => "/admin/sign_out"
+    get '/admin' => "homes#top"
+  end
+
+  namespace :admin do
+    get '/admin/sign_in' => "sessions#new"
+    post '/admin/sign_in' => "sessions#create"
+    delete '/admin/sign_out' => "sessions#destroy"
   end
 
   devise_for :customers
