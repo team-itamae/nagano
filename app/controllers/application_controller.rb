e7d86d45db7184abd254fe7dbfd2e4fb5998fe9e
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-08-20 12:16:05 +0000

    [Finish] 退会処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
    
+  def after_sign_up_path_for(resource)
+    customers_path
+  end
+   
   def after_sign_in_path_for(resource)
-    customers_show_path(current_customer.id)
+    root_path
   end
 
   def after_sign_out_path_for(resource)
