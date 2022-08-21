aee51d942bd81bc5405299e59cc0a993f771c40c
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-08-21 09:21:02 +0000

    [Finish] 会員編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,9 +3,13 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=current_customer
   end
 
   def update
+    @customer=current_customer
+    @customer.update(customer_params)
+    redirect_to customers_path
   end
 
   def unsubscribe
@@ -15,5 +19,11 @@ class Public::CustomersController < ApplicationController
     current_customer.update(is_deleted: true)
     reset_session
     redirect_to  root_path
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:email, :name_sei, :name_mei, :kana_sei, :kana_mei, :post_code, :address, :phone_number, :is_deleted)
   end
 end
