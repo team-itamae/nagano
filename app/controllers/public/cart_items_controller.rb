d19af954c3d11903fe0f4a907192d47171459260
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-22 09:14:07 +0000

    [update]cart_add

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -21,17 +21,17 @@ class Public::CartItemsController < ApplicationController
     @cart_item.save
     redirect_to cart_items_path
     
-      if @cart_item.present?
-            @cart_item.quantity += params[:cart_item][:quantity].to_i
-            @cart_item.save
-            redirect_to cart_items_path
+      # if @cart_item.present?
+      #       @cart_item.quantity += params[:cart_item][:quantity].to_i
+      #       @cart_item.save
+      #       redirect_to cart_items_path
   
-      elsif @cart_item.save
-            @cart_items = current_customer.cart_items.all
-            render 'index'
-      else　
-            render 'index'
-      end
+      # elsif @cart_item.save
+      #       @cart_items = current_customer.cart_items.all
+      #       render 'index'
+      # else　
+      #       render 'index'
+      # end
        
   end
 
