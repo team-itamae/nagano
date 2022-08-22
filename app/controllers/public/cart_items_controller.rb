ab1989e196d96f46640fdf17b3f7a243f6a22b6d
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-22 01:59:55 +0000

    [update]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = cart_items.all
   end
 
   def update
@@ -12,5 +13,27 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+        @cart_item = cart_items.new(cart_item_params)
+ 
+        if cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+
+            cart_item = cart_items.find_by(item_id: params[:cart_item][:item_id])
+            cart_item.quantity += params[:cart_item][:quantity].to_i
+
+            cart_item.save
+            redirect_to cart_items_path
+
+        elsif @cart_item.save
+            　@cart_items = cart_items.all
+            　render 'index'
+        else　
+            render 'index'
+        end
   end
+  
+  
+private
+    def cart_item_params
+        params.require(:cart_item).permit(:item_id, :price, :quantity)
+    end
 end
