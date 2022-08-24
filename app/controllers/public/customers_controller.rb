54a11d15f7ed3ef6da30d65da7db62d5683f3122
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-24 07:42:21 +0000

    kunishima

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -23,10 +23,4 @@ class Public::CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:email, :name_sei, :name_mei, :kana_sei, :kana_mei, :post_code, :address, :phone_number, :is_deleted)
   end
-  
-  private
-  
-  def customer_params
-    params.require(:customer).permit(:email, :name_sei, :name_mei, :kana_sei, :kana_mei, :post_code, :address, :phone_number, :is_deleted)
-  end
 end
