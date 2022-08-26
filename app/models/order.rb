028864025480d4752163f66ecd9346ae156bf12d
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-08-26 05:30:36 +0000

    header編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,15 +4,10 @@ class Order < ApplicationRecord
   has_many :order_details
   has_many :items, through: :order_details
 
+
   enum status: {awaiting_payment: 0,confirm_payment: 1,in_production: 2,ship_preparation: 3,shipped: 4}
   enum payment_method: {credit_card: 0,transfer: 1}
 
-
-    def statuses
-      statuses.keys.map do |s|
-        [ApplicationController.helpers.t("status.order.#{name.downcase}.#{s}"), s]
-      end
-    end
 
   def subtotal
     item.add_tax_price * quantity
